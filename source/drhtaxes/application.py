668b6bf56da6a3d0f26b6c8cd72274619b6e8bb0
Geoff Handsfield <g.handsfield@gmail.com>
2017-06-09 11:07:58 +1200

    started pulling individual tax calc module

--- source/drhtaxes/application.py
+++ source/drhtaxes/application.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
-
+from drhtaxes.individual import Tax
 def main():
     print('Application DRH Taxes')
+    income = 5
+    in_tax = Tax()
+    indi_calc_tax = in_tax.calc_tax(income)
+    print(indi_calc_tax)
 
 if __name__ == '__main__':
     main()
